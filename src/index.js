933faeef60d1a9b5dddb29ce60a792836f2f3d12
VladAverin <Vladaverin@users.noreply.github.com>
2021-03-15 15:13:15 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array.length == 0) {
+    return 0;
+  } else if (array.length !== 0) {
+    return Math.min.apply(null, array);
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array.length == 0) {
+    return 0;
+  } else {
+    return Math.max.apply(null, array);
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  let sum = array.reduce((a, b) => a + b, 0);
+  let result = sum / array.length;
+  return result;
 }
